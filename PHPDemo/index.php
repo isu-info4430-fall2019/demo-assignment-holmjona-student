905a7a3b8338c903cb7947c209b85e740c7ac5f8
Jon Holmes <holmjona@isu.edu>
2019-09-16 14:50:38 -0600

    Added some discussion about variables and includes

---
+++ PHPDemo/index.php
@@ -0,0 +1,22 @@
+<?php
+
+echo 'Hello Framework!';
+
+print "Hello";
+
+echo "Hello", " World", " asdf", "   ;alkjd;alf";
+
+
+$variable = "something else";
+var_dump($variable);
+
+include("one.php");
+
+
+$variable = 7412;
+var_dump($variable);
+
+
+
+
+?>
---
+++ PHPDemo/one.php
@@ -0,0 +1,4 @@
+<?php
+echo "<p>I was included $variables</p>";
+echo '<p>I am another string $variable</p>';
+?>
